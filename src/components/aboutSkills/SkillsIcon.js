eb6d676d959b1903a9753893d75d00c9fd79342f
Breana <brecjohnson@gmail.com>
2023-02-16 06:42:27 -0500

    cleaned up code and updated padding

--- src/components/aboutSkills/SkillsIcon.js
+++ src/components/aboutSkills/SkillsIcon.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function SkillsIcon({ src, alt }) {
   return (
     <div >
-      <Image src={src} alt={alt}  height={90} />
+      <Image src={src} alt={alt} height={90} />
     </div>
   );
 }
