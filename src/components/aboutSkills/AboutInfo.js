d2c1d3d799295e5d1912197451e5d6e8e6142255
Breana <brecjohnson@gmail.com>
2023-02-18 14:58:07 -0500

    added animations -- skills btn

--- src/components/aboutSkills/AboutInfo.js
+++ src/components/aboutSkills/AboutInfo.js
@@ -1,7 +1,25 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useInView,
+  useSpring,
+} from "framer-motion"
 
 export default function AboutInfo() {
     const [mobile, setMobile] = useState(undefined)
+
+    const { scrollYProgress } = useScroll();
+
+  const x = useSpring(useTransform(scrollYProgress, [0, 1], [-1300, 0]), {
+    stiffness: 100,
+    damping: 25,
+    restDelta: 0.001,
+  });
+
+  const ref = useRef(null);
+  const isInView = useInView(ref);
     
     useEffect(() => {
         const updateMobile = () => {
@@ -18,7 +36,8 @@ export default function AboutInfo() {
 
   return (
     <div>
-      
+
+
       {mobile ? (
       <div>
         <p className="p-4 text-center text-sm pt-14">
@@ -26,8 +45,13 @@ export default function AboutInfo() {
         </p>
       </div>
       ) : (
-        <div>
-            <p className="p-10 text-center pt-14 md:text-lg lg:text-xl lg:px-20 lg:pt-24 xl:text-2xl xl:px-36">
+        // slide in from left
+        <div className='pt-14'>
+                <div className="font-zidan m-3 ml-12 text-6xl">
+          {'<About />'}
+      </div>
+            <p className="p-10 text-center  md:text-lg lg:text-xl lg:px-20 lg:pt-16 xl:text-2xl xl:px-36">
+              {/* FADE IN ⬇️ */}
             I am a full-stack software engineer that specializes in developing enjoyable user experiences. Javascript is my primary superpower as I tackle the world of the web. I leverage my <span className="underline">artistic and graphic design</span> skills to create simple, beautiful, and intuitive layouts for applications and websites. When I&apos;m not centering divs, you&apos;ll find me painting, drawing, or working out.
             </p>
         </div>
