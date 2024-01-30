dd962ffb8d7f1cf503721578c2e859f00fbf3f76
Breana <brecjohnson@gmail.com>
2024-01-30 13:30:19 -0500

    updated supporting text for netflixclone project

--- src/pages/projects/netflix-clone.jsx
+++ src/pages/projects/netflix-clone.jsx
@@ -1,9 +1,17 @@
 export default function PocketNYC() {
   return (
-    <div className="h-screen mt-16">
-      <h1 className="text-[#6352ff] text-center text-4xl font-bold  mb-4">
+    <div className="h-full sm:m-16 m-6 mt-16">
+      <h1 className="text-center text-5xl font-bold mb-4 tracking-wide">
         Netflix Clone
       </h1>
+      <p className="font-zidan text-[#6352ff] text-2xl tracking-widest
+      md:text-3xl">
+        TLDR;
+      </p>
+      <p className="ml-4 sm:ml-6 text-sm
+      sm:text-md md:text-xl">
+        {`I completed this Netflix clone as solo bootcamp project during my time at FullStack Academy. The goal of this project was to familiarize myself with front-end development, querying APIs, and implementing user authentication. Take a look at the video below for an  overview of the app!`}
+      </p>
       <div className="mt-8 flex justify-center">
         <iframe
           width="1120"
