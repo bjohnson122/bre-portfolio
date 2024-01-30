5157a2719f8345a63778b946335475ed8e28c8b7
Breana <brecjohnson@gmail.com>
2024-01-30 13:56:59 -0500

    fixed styling on both apps

--- src/pages/projects/netflix-clone.jsx
+++ src/pages/projects/netflix-clone.jsx
@@ -1,21 +1,23 @@
 export default function PocketNYC() {
   return (
-    <div className="h-full sm:m-16 m-6 mt-16">
+    <div className="h-screen sm:m-16 m-6 mt-16">
       <h1 className="text-center text-5xl font-bold mb-4 tracking-wide">
         Netflix Clone
       </h1>
-      <p className="font-zidan text-[#6352ff] text-2xl tracking-widest
-      md:text-3xl">
+      <p
+        className="font-zidan text-[#6352ff] text-2xl tracking-widest
+      md:text-3xl"
+      >
         TLDR;
       </p>
-      <p className="ml-4 sm:ml-6 text-sm
-      sm:text-md md:text-xl">
+      <p
+        className="ml-4 sm:ml-6 text-sm
+      sm:text-md md:text-xl"
+      >
         {`I completed this Netflix clone as solo bootcamp project during my time at FullStack Academy. The goal of this project was to familiarize myself with front-end development, querying APIs, and implementing user authentication. Take a look at the video below for an  overview of the app!`}
       </p>
-      <div className="mt-8 flex justify-center">
+      <div className="mt-8 justify-center aspect-w-16 aspect-h-9 w-full px-[25%] lg:scale-90">
         <iframe
-          width="1120"
-          height="630"
           src="https://www.youtube.com/embed/NwqUgWcSoIg?si=9vc_Xeebnv0J82zt&hd=1"
           title="Netflix Clone - Video Player"
           frameborder=""
